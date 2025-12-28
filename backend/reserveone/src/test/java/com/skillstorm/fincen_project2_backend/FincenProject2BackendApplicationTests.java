e46374d8b9bf8b244f40477288159ec12e0bc195
Joshua Thompson <warlord84502@yahoo.com>
2025-12-28 14:41:32 -0600

    feat(frontend): scaffold Angular app in frontend directory

---
+++ backend/reserveone/src/test/java/com/skillstorm/fincen_project2_backend/FincenProject2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.fincen_project2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincenProject2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
