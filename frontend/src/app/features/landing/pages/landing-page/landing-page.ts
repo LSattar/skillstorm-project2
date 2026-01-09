07b8d543dc1b1c6e446f5dc4464748b408839dcc
Leah Sattar <143446653+LSattar@users.noreply.github.com>
2026-01-09 12:32:57 -0500

    Update landing-page.ts

--- frontend/src/app/features/landing/pages/landing-page/landing-page.ts
+++ frontend/src/app/features/landing/pages/landing-page/landing-page.ts
@@ -16,7 +16,11 @@ import { HotelResponse } from '../../services/hotel.service';
   standalone: true,
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   imports: [CommonModule, FormsModule, Header, Footer, UserProfileModal, SystemSettingsModal],
+=======
+  imports: [CommonModule, FormsModule, Header, Footer, UserProfileModal, RoomSearchModal, RoomSearchResults],
+>>>>>>> Stashed changes
 =======
   imports: [CommonModule, FormsModule, Header, Footer, UserProfileModal, RoomSearchModal, RoomSearchResults],
 >>>>>>> Stashed changes
